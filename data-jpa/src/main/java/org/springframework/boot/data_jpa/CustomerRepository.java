d3ac86346eeec76e85a7e797957e4b0f3cffe2bc
bek <bek@Bekjan-Linux>
2017-11-23 13:05:00 +0600

    initial

---
+++ data-jpa/src/main/java/org/springframework/boot/data_jpa/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.springframework.boot.data_jpa;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+	List<Customer> findByLastName(String lastName);
+}
