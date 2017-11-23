bbdc7ba32ab6a64363b7eef4df320ab76616bb36
bek <bek@Bekjan-Linux>
2017-11-23 11:44:29 +0600

    initial

---
+++ helloworld/src/main/java/org/springframework/boot/helloworld/App.java
@@ -0,0 +1,33 @@
+package org.springframework.boot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+@Controller
+@EnableAutoConfiguration
+public class App 
+{
+	
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World";
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
